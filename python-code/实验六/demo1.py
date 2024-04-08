cbed1c47f2818fe957b6cec3eac198ee4c9da34b
2627067561 <2627067561@qq.com>
2024-04-08 10:45:26 +0000

    test

--- python-code/实验六/demo1.py
+++ python-code/实验六/demo1.py
@@ -1,3 +1,12 @@
-print("test")
-print(123)
-print("test")
+item=[11,22,33,44,55,66,77,88,99]
+item1=[]
+item2=[]
+items={'k1':'','k2':''}
+for i in item:
+    if i<=66:
+        item1.append(i)
+    else:
+        item2.append(i)
+items['k1']=item1
+items['k2']=item2
+print(items)
